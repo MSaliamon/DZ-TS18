ca84e390e52e30d6c5e44f97b3ae415fe69ef682
MSaliamon <maksimsalamon181@gmail.com>
2024-03-24 18:48:47 +0200

    first commit

---
+++ src/app.ts
@@ -0,0 +1,100 @@
+
+// type Award = string;
+
+// interface Movie {
+//     title: string;
+//     releaseYear: number;
+//     rating: number;
+//     awards: Award[];
+// }
+
+// interface Category {
+//     name: string;
+//     movies: Movie[];
+// }
+
+// interface MatchFilter {
+//     filter: string;
+// }
+
+// interface RangeFilter {
+//     filter: number;
+//     filterTo: number;
+// }
+
+// interface ValuesFilter {
+//     values: string[];
+// }
+
+// type Filter = MatchFilter | RangeFilter | ValuesFilter;
+
+// interface FiltersState {
+//     matchFilter?: MatchFilter;
+//     rangeFilter?: RangeFilter;
+//     valuesFilter?: ValuesFilter;
+// }
+
+type Award = string;
+
+interface Movie {
+    title: string;
+    releaseYear: number;
+    rating: number;
+    awards: Award[];
+  }
+  
+  interface Category {
+    name: string;
+    movies: Movie[];
+  }
+  
+  interface MatchFilter {
+    filter: string;
+  }
+  
+  interface RangeFilter {
+    filter: number;
+    filterTo: number;
+  }
+  
+  interface ValuesFilter {
+    values: string[];
+  }
+  
+  type Filter = MatchFilter | RangeFilter | ValuesFilter;
+  
+  class MovieList {
+    private movies: Movie[];
+    private filters: Filter[];
+  
+    constructor() {
+      this.movies = [];
+      this.filters = [];
+    }
+  
+    applySearchValue(filter: MatchFilter): void {
+      
+    }
+  
+    applyFiltersValue(filters: Filter[]): void {
+      
+    }
+  }
+  
+  class CategoryList {
+    private categories: Category[];
+    private filters: Filter[];
+  
+    constructor() {
+      this.categories = [];
+      this.filters = [];
+    }
+  
+    applySearchValue(filter: MatchFilter): void {
+      
+    }
+  
+    applyFiltersValue(filters: Filter[]): void {
+      
+    }
+  }
